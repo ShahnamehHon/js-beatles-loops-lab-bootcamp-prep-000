9d31b9f938f1fa3268694fbfb8ab43b6c3462be2
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-18 15:51:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function theBeatlesPlay (arrayMusicians, arrayInstruments) {
   
   for (let i = 0; i < arrayMusicians.length; i++) {
     
-    arr.push(`${arrayMusicians} plays ${arrayInstruments}`);
+    arr.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
   }
 }
